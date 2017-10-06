85e163290b35ca529918f1a0153c95799489e19a
S.Percentage <Syn.Tri.Naga@gmail.com>
2017-10-06 22:57:29 +0900

    added main window

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,31 @@
 
+#[macro_use] extern crate appinstance;
+extern crate libc;
+extern crate ferrite;
+extern crate ws_common;
+
+use ws_common::{NativeWindow, WindowServer};
+
+pub struct Application { main_window: NativeWindow }
+impl Application
+{
+    AppInstance!(pub static instance: Application = Application::new());
+
+    const INITIAL_SIZE: (u16, u16) = (960, 960 * 9 / 16);
+    fn new() -> Self
+    {
+        let main_window = NativeWindow::new(Self::INITIAL_SIZE, "DigitalCampus 2017");
+        main_window.show();
+        Application { main_window }
+    }
+    fn process_events(&self)
+    {
+        WindowServer::instance().process_events();
+    }
+}
+
 fn main()
 {
     println!("=== DIGITAL CAMPUS 2017 ===");
+    Application::instance().process_events();
 }
