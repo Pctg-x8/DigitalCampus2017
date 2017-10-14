850133feebf1679c6ff7a56e1a9a63b12b6ffa53
S.Percentage <Syn.Tri.Naga@gmail.com>
2017-10-14 16:30:37 +0900

    fixed errors

--- src/event.rs
+++ src/event.rs
@@ -1,7 +1,7 @@
 //! platform dependent event signaling implementation
 
-#[cfg(unix)] use std::os::unix::{RawFd, AsRawFd};
-use std::ptr::null;
+#[cfg(unix)] use std::os::unix::prelude::{RawFd, AsRawFd};
+#[cfg(windows)] use std::ptr::null;
 
 #[cfg(windows)] pub struct Event(::winapi::um::handleapi::HANDLE);
 #[cfg(unix)] pub struct Event(RawFd);
@@ -20,12 +20,12 @@ impl Event
 	pub fn set(&self)
 	{
 		#[cfg(windows)] unsafe { ::winapi::um::handleapi::SetEvent(self.0); }
-		#[cfg(unix)] unsafe { let inc: u64 = 1; ::libc::write(self.0, ::std::mem::transmute::<_, *const [u8; 8]>(&inc), 8); }
+		#[cfg(unix)] unsafe { let inc: u64 = 1; ::libc::write(self.0, ::std::mem::transmute::<_, *const _>(&inc), 8); }
 	}
 	pub fn wait(&self)
 	{
 		#[cfg(windows)] unsafe { ::winapi::um::synchapi::WaitForSingleObject(self.0, ::winapi::um::synchapi::INFINITE); }
-		#[cfg(unix)] unsafe { let mut inc: [u8; 8] = [0; 8]; ::libc::read(self.0, inc.as_ptr(), 8); }
+		#[cfg(unix)] unsafe { let mut inc: [u8; 8] = [0; 8]; ::libc::read(self.0, inc.as_ptr() as _, 8); }
 	}
 }
 
