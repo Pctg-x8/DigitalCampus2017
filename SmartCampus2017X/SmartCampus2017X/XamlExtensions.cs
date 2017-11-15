9f84d8c393a161431d2ccb23d55ac077f683c745
Pctg-x8 <Syn.Tri.Naga@gmail.com>
2017-11-15 23:35:56 +0900

    added translation to ja, zh(hans) and kr (中国語(繁体字)、韓国語は曜日以外Google翻訳です)

---
+++ SmartCampus2017X/SmartCampus2017X/XamlExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SmartCampus2017X.Extensions.Xaml
+{
+    [ContentProperty("Key")]
+    public sealed class Translate : IMarkupExtension
+    {
+        public string Key { get; set; }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (string.IsNullOrEmpty(this.Key)) throw new ArgumentNullException("Key");
+            
+            return AppResources.ResourceManager.GetString(this.Key) ?? $"??{this.Key}??";
+        }
+    }
+}
